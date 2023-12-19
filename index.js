33193e3a16a4afcbcef35682549313e631063315
Marc McBurney <marcmcb@yandex.com>
2023-12-19 19:41:51 +0200

    modified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 8080;
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}`)
